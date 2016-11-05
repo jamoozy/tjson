1df43e0f06c64c6a8bc0426dce75f9382c7c6e77
Andrew "Jamoozy" Sabisch <jamoozy@gmail.com>
2016-11-05 14:46:23 -0700

    Linted and updated.

--- errors.go
+++ errors.go
@@ -4,30 +4,33 @@ import (
 	"errors"
 )
 
-type TJSONError error
+// Error is a TJSON-specific error that Marshal can return.
+type Error error
 
+// These are all the errors that malformatted TJSON can result in.  Note that JSON errors can also
+// be returned by the functions in this library.
 var (
-	ErrMissingTag   TJSONError = errors.New("Key missing a tag.")
-	ErrDuplicateKey TJSONError = errors.New("Duplicate key.")
+	ErrMissingTag   Error = errors.New("Key missing a tag.")
+	ErrDuplicateKey Error = errors.New("Duplicate key.")
 
-	ErrB16UpperCase TJSONError = errors.New("Base16 data contains upper case letters.")
-	ErrB16Invalid   TJSONError = errors.New("Invalid base16-encoded data.")
+	ErrB16UpperCase Error = errors.New("Base16 data contains upper case letters.")
+	ErrB16Invalid   Error = errors.New("Invalid base16-encoded data.")
 
-	ErrB32UpperCase TJSONError = errors.New("Base32 data contains upper case letters.")
-	ErrB32Invalid   TJSONError = errors.New("Invalid base32-encoded data.")
-	ErrB32Padding   TJSONError = errors.New("Padding in base32-encoded data.")
+	ErrB32UpperCase Error = errors.New("Base32 data contains upper case letters.")
+	ErrB32Invalid   Error = errors.New("Invalid base32-encoded data.")
+	ErrB32Padding   Error = errors.New("Padding in base32-encoded data.")
 
-	ErrB64UpperCase TJSONError = errors.New("Base64 data contains upper case letters.")
-	ErrB64Invalid   TJSONError = errors.New("Invalid base64-encoded data.")
-	ErrB64Padding   TJSONError = errors.New("Padding in base64-encoded data.")
+	ErrB64UpperCase Error = errors.New("Base64 data contains upper case letters.")
+	ErrB64Invalid   Error = errors.New("Invalid base64-encoded data.")
+	ErrB64Padding   Error = errors.New("Padding in base64-encoded data.")
 
-	ErrIntOverflow  TJSONError = errors.New("Integer overflow.")
-	ErrIntUnderflow TJSONError = errors.New("Integer underflow.")
-	ErrIntInvalid   TJSONError = errors.New("Invalid integer.")
+	ErrIntOverflow  Error = errors.New("Integer overflow.")
+	ErrIntUnderflow Error = errors.New("Integer underflow.")
+	ErrIntInvalid   Error = errors.New("Invalid integer.")
 
-	ErrUintOverflow TJSONError = errors.New("Unsigned integer overflow.")
-	ErrUintNegative TJSONError = errors.New("Unsigned integer negative.")
-	ErrUintInvalid  TJSONError = errors.New("Invalid unsigned integer.")
+	ErrUintOverflow Error = errors.New("Unsigned integer overflow.")
+	ErrUintNegative Error = errors.New("Unsigned integer negative.")
+	ErrUintInvalid  Error = errors.New("Invalid unsigned integer.")
 
-	ErrTimeInvalid TJSONError = errors.New("Invalid time format.")
+	ErrTimeInvalid Error = errors.New("Invalid time format.")
 )
